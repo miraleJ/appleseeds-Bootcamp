2c17a97a0a329854265c11f9bfa69d9d6f6d0999
miraleJ <miraleJ@users.noreply.github.com>
2021-02-07 15:07:39 +0200

    ex 1.1 - variables, types

---
+++ js/ex1.1.js
@@ -0,0 +1,20 @@
+let s ='hello world';
+let n = 4;
+
+let o = {
+    name: "Ami",
+    age: 8
+};
+
+let b = true;
+let a = null;
+let c;
+let sym = Symbol(o);
+
+console.log(typeof(s));
+console.log(typeof(n));
+console.log(typeof(o));
+console.log(typeof(b));
+console.log(typeof(a));
+console.log(typeof(c));
+console.log(typeof(sym));
