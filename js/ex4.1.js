b33f6db44b22f78a42ddb47749d9bb1ce70894fe
miraleJ <miraleJ@users.noreply.github.com>
2021-02-08 14:40:24 +0200

    ex4.1

---
+++ js/ex4.1.js
@@ -0,0 +1,23 @@
+let numSiblings = window.prompt('How many siblings do you have?');
+if (numSiblings == 1) {
+    console.log('1 sibling!');
+} else if (numSiblings > 1) {
+    console.log('More than 1 sibling!');
+} else
+    console.log('No siblings');
+
+ numSiblings = window.prompt('How many siblings do you have?');
+if (numSiblings === 1) {
+    console.log('1 sibling!');
+} else if (numSiblings > 1) {
+    console.log('More than 1 sibling!');
+} else
+    console.log('No siblings');
+
+ numSiblings = parseInt(window.prompt('How many siblings do you have?'));
+if (numSiblings === 1) {
+    console.log('1 sibling!');
+} else if (numSiblings > 1) {
+    console.log('More than 1 sibling!');
+} else
+    console.log('No siblings');
