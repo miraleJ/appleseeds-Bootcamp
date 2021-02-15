ad46ce54e08eba5d8aaadb66011ade4a4b3114c8
miraleJ <miraleJ@users.noreply.github.com>
2021-02-15 23:06:55 +0200

    ex11.1

---
+++ js/ex11.1.js
@@ -0,0 +1,21 @@
+var library = [
+    {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+    },
+    {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+    },
+    {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+    }
+    ];
+
+const booksForReading = library => console.log(library.filter(book => book.readingStatus));
+
+booksForReading(library);
