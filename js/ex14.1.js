c9bb7ed8bc01019b93b1847b49fb75394bac2344
miraleJ <miraleJ@users.noreply.github.com>
2021-02-16 16:35:06 +0200

    ex14.1

---
+++ js/ex14.1.js
@@ -0,0 +1,17 @@
+function getSum(arr1, arr2){
+    let sum = 0;
+    for (let i=0; i < arr1.length; i++){
+    sum += arr1[i];
+    }
+    for (let i=0; i < arr2.length; i++){
+    sum += arr2[i];
+    }
+    }
+    getSum([1,2,3],[5,66,23]);
+
+//row 3
+//the calling to the function is not good (,)
+
+    // row 4
+    // the log of the terminal
+    // sum is defined as const and cann't be reassigned
