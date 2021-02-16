fd3e67ecc79afcc328a59b96a4b8bf97d51df4da
miraleJ <miraleJ@users.noreply.github.com>
2021-02-16 16:56:18 +0200

    ex14.4

---
+++ js/ex14.4.js
@@ -0,0 +1,12 @@
+function calcAverage (arr){
+    var sum =0;
+    for ( var i = 0 ; i < arr .length; i ++ ){
+    sum += arr [ i ];
+    }
+    return sum ;
+    }
+    console.log(calcAverage ([ 85 , 90 , 92 ]));
+
+    //2
+    //console.log
+    // the sum variable was not zeroed
