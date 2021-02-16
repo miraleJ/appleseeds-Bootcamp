009acea30a084a0520f70bc9a56f838667fb95f9
miraleJ <miraleJ@users.noreply.github.com>
2021-02-16 16:52:07 +0200

    ex14.3

---
+++ js/ex14.3.js
@@ -0,0 +1,9 @@
+function getSumOfEven(arr){
+    return arr[0] + arr[2] + arr[4] + arr[6] + arr[8];
+   }
+  console.log(getSumOfEven([1,2,3,4,5,6,7,8,9,10]));
+
+
+  ///// 2
+  // console.log
+  // the calling to the array is going out of the scope of the array
